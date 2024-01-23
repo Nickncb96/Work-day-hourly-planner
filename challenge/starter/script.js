d61aa8cc623cc206ac3983b8be8455ae20844799
nickncb96 <nicholasncb12@hotmail.com>
2024-01-23 14:23:27 +0000

    fixing the updatecoloringcoding function within the script.js to ensure the time blocks are coded accordingly as the hours pass throughout the working day hours

--- challenge/starter/script.js
+++ challenge/starter/script.js
@@ -5,20 +5,29 @@ $(document).ready(function () {
   // timeblocks for standard business hours (Monday to Friday, 9 am to 5 pm)
   for (let hour = 9; hour <= 17; hour++) {
     let timeBlock = $("<div>").addClass("row time-block");
-    timeBlock.append($("<div>").addClass("col-md-1 hour").attr("data-day", dayjs().day() || 7).text(dayjs().hour(hour).format("h A")));
+    let hourDisplay = dayjs().hour(hour).format("h A");
+    let currentHour = dayjs().hour();
+    
+    timeBlock.append($("<div>").addClass("col-md-1 hour").text(hourDisplay));
     timeBlock.append($("<textarea>").addClass("col-md-10"));
     let saveButton = $("<button>").addClass("col-md-1 saveBtn").html('<i class="fas fa-save"></i>');
     saveButton.attr('title', 'Save'); // Tooltip text
     timeBlock.append(saveButton);
 
     $(".container").append(timeBlock);
+
+    // Color code each time block based on past, present, and future
+    if (hour < currentHour) {
+      timeBlock.addClass("past");
+    } else if (hour === currentHour) {
+      timeBlock.addClass("present");
+    } else {
+      timeBlock.addClass("future");
+    }
   }
 
   // Load events from local storage
   loadEvents();
-
-  // logic for color-coding timeblocks (past, present, future)
-  updateColorCoding();
 
   // Make textareas draggable using jQuery UI
   $("textarea").draggable();
@@ -28,31 +37,11 @@ $(document).ready(function () {
     // Get the text value, hour, and day of the clicked save button's parent timeblock
     let eventText = $(this).siblings("textarea").val();
     let eventHour = parseInt($(this).siblings(".hour").text());
-    let eventDay = dayjs().day() || 7; // Adjust for Sunday
 
     // Save the event to local storage
-    saveEvent(eventDay, eventHour, eventText);
+    saveEvent(eventHour, eventText);
   });
 });
-
-// Function to update color coding of timeblocks
-function updateColorCoding() {
-  let currentHour = dayjs().hour();
-  let currentDay = dayjs().day() || 7; // 
-
-  $(".time-block").each(function () {
-    let blockHour = parseInt($(this).find(".hour").text());
-    let blockDay = parseInt($(this).find(".hour").attr("data-day"));
-
-    if (blockDay < currentDay || (blockDay === currentDay && blockHour < currentHour)) {
-      $(this).removeClass("present future").addClass("past");
-    } else if (blockDay === currentDay && blockHour === currentHour) {
-      $(this).removeClass("past future").addClass("present");
-    } else {
-      $(this).removeClass("past present").addClass("future");
-    }
-  });
-}
 
 // Function to load events from local storage
 function loadEvents() {
@@ -62,12 +51,11 @@ function loadEvents() {
   if (savedEvents) {
     // Loop through saved events and display them in corresponding timeblocks
     for (let i = 0; i < savedEvents.length; i++) {
-      let eventDay = savedEvents[i].day;
       let eventHour = savedEvents[i].hour;
       let eventText = savedEvents[i].text;
 
       $(".time-block").each(function () {
-        if (parseInt($(this).find(".hour").text()) === eventHour && (dayjs().day() || 7) === eventDay) {
+        if (parseInt($(this).find(".hour").text()) === eventHour) {
           $(this).find("textarea").val(eventText);
         }
       });
@@ -76,22 +64,21 @@ function loadEvents() {
 }
 
 // Function to save events to local storage
-function saveEvent(day, hour, text) {
+function saveEvent(hour, text) {
   // Retrieve existing events from local storage
   let savedEvents = JSON.parse(localStorage.getItem("events")) || [];
 
-  // Check if there's already an event saved for the current day and hour
-  let existingEventIndex = savedEvents.findIndex((event) => event.day === day && event.hour === hour);
+  // Check if there's already an event saved for the current hour
+  let existingEventIndex = savedEvents.findIndex((event) => event.hour === hour);
 
   if (existingEventIndex !== -1) {
     // Update existing event
     savedEvents[existingEventIndex].text = text;
   } else {
     // Add new event
-    savedEvents.push({ day, hour, text });
+    savedEvents.push({ hour, text });
   }
 
   // Save updated events to local storage
   localStorage.setItem("events", JSON.stringify(savedEvents));
 }
-
